925138369dfee80488db58c177c62ab7344ecc15
Arsene Mpuhwezimana <mpuhwearsene@gmail.com>
2017-01-11 14:34:58 +0200

    I created a hash which work as a store. There are three items in my store.

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+Item1 = {item: "Phone", color: "white", price: "200$", brand: "Tecno", name: "Phantom 6"}
+Item2 = {"item" => "computer", "color" => "black", "price" => "600$", "brand" => "Apple", "name" => "Mac Book"}
+Item3 = {"item" => "Car", "color" => "Purple", "price" => "10000$", "brand" => "Chevrolet", "name" => "Pick Up"}
+
+puts Item2["item"]
