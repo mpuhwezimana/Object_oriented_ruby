2edf4192c92a25cd1f2084912ecc0af8a85a1676
Arsene Mpuhwezimana <mpuhwearsene@gmail.com>
2017-01-11 15:41:02 +0200

    I changed the code into OOP. I changed from hashes to class.

--- store_item.rb
+++ store_item.rb
@@ -1,5 +1,43 @@
-Item1 = {item: "Phone", color: "white", price: "200$", brand: "Tecno", name: "Phantom 6"}
-Item2 = {"item" => "computer", "color" => "black", "price" => "600$", "brand" => "Apple", "name" => "Mac Book"}
-Item3 = {"item" => "Car", "color" => "Purple", "price" => "10000$", "brand" => "Chevrolet", "name" => "Pick Up"}
+class Store
 
-puts Item2["item"]
+  def initialize(item, color, price, brand, name)
+    @item = item    
+    @color = color
+    @price = price
+    @brand = brand   
+    @name = name 
+  end  
+
+  def item
+    @item
+  end 
+
+  def color
+    @color
+  end 
+
+  def price
+    @price 
+  end 
+
+  def brand
+    @brand
+  end 
+
+  def name
+    @name
+  end 
+
+  def info
+    "This is a #{item} which is #{color} and it cost #{price}. Its brand is #{brand} and the name is #{name}"
+  end
+
+end 
+
+item1 = Store.new("Phone", "white", "200$", "Tecno", "Phantom 6")
+item2 = Store.new("computer", "black", "600$", "Apple", "Mac Book")
+item3 = Store.new("Car", "Purple", "10000$", "Chevrolet", "Pick Up")
+
+puts item3.info
+puts item2.info
+puts item1.info
