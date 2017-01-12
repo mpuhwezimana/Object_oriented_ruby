26c680d3ca037b52825686c38b49b1c3263bf78e
Arsene Mpuhwezimana <mpuhwearsene@gmail.com>
2017-01-12 12:20:30 +0200

    I refactored my code in order to make it more simple. I used attr_accessor instead of using many methods. I also used a hash instead of many argmuents.

--- store_item.rb
+++ store_item.rb
@@ -1,43 +1,25 @@
 class Store
 
-  def initialize(item, color, price, brand, name)
-    @item = item    
-    @color = color
-    @price = price
-    @brand = brand   
-    @name = name 
+  attr_accessor :item, :color, :price, :brand, :name 
+
+  def initialize(item_hash)
+    @item = item_hash[:item] 
+    @color = item_hash[:color]
+    @price = item_hash[:price]
+    @brand = item_hash[:brand]
+    @name = item_hash[:name]
   end  
-
-  def item
-    @item
-  end 
-
-  def color
-    @color
-  end 
-
-  def price
-    @price 
-  end 
-
-  def brand
-    @brand
-  end 
-
-  def name
-    @name
-  end 
 
   def info
     "This is a #{item} which is #{color} and it cost #{price}. Its brand is #{brand} and the name is #{name}"
   end
-
 end 
 
-item1 = Store.new("Phone", "white", "200$", "Tecno", "Phantom 6")
-item2 = Store.new("computer", "black", "600$", "Apple", "Mac Book")
-item3 = Store.new("Car", "Purple", "10000$", "Chevrolet", "Pick Up")
+item1 = Store.new({item: "Phone", color: "white", price: 200, brand: "Tecno", name: "Phantom 6"})
+item2 = Store.new(item: "computer", color: "black", price: "600$", brand: "Apple", name: "Mac Book")
+item3 = Store.new(item: "Car", color: "Purple", price: "10000$", brand: "Chevrolet", name: "Pick Up")
 
 puts item3.info
+item2.price = "300$"
 puts item2.info
 puts item1.info
